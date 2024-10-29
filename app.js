f432605f4288b9f4e4f0ef316d805965770421eb
hussTech44 <hussein.thebian@techvitis.com>
2024-10-29 16:59:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Nabil wanted it to be simple!!!");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Default to 3000 if no port is specified
+
+app.get('/', (req, res) => {
+    res.send('Nabil wanted it to be simple!!!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
